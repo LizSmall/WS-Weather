d5fc9bd9437d857ef42e92e034f69f8ed45a6aa2
WSmall <wsmall86@gmail.com>
2025-07-21 17:56:10 -0400

    Update apiKey

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+function searchCity(city) {
+  let apiKey = "31be6t3394f370982af3d7o35730974f";
+  let apiUrl = 
+  "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial";
+  console.log(apiUrl);
+}
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
   
 }
 
