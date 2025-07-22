5f3da970ebb1a4163ae93ab9b05204a1c1f1b35e
WSmall <wsmall86@gmail.com>
2025-07-22 17:40:20 -0400

    Added API search functionality

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
+function refreshWeather(response) {
+  // console.log(response.data);
+  // console.log(response.data.temperature.current);
+ let temperatureElement = document.querySelector("#temperature");
+  // temperatureElement.innerHTML = response.data.temperature.current;
+ let temperature = response.data.temperature.current; 
+ let cityElement = document.querySelector("#city");
+  
+ cityElement.innerHTML = response.data.city;
+ temperatureElement.innerHTML = Math.round(temperature);
+ 
+
+}
+
 function searchCity(city) {
   let apiKey = "31be6t3394f370982af3d7o35730974f";
   let apiUrl = 
   `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-  console.log(apiUrl);
+  // console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+
+
 }
 
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
   
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("New York");
