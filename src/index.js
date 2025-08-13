260dc960a818d0a751e2a717a34641d28b688632
WSmall <wsmall86@gmail.com>
2025-08-13 12:38:18 -0400

    Corrected search city function

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ function refreshWeather(response) {
   // temperatureElement.innerHTML = response.data.temperature.current;
  let temperature = response.data.temperature.current; 
  let cityElement = document.querySelector("#city");
-  
+  // console.log(response.data.condition.description);
+
  cityElement.innerHTML = response.data.city;
  temperatureElement.innerHTML = Math.round(temperature);
- 
+ }
 
-}
 
 function searchCity(city) {
   let apiKey = "31be6t3394f370982af3d7o35730974f";
@@ -18,17 +18,14 @@ function searchCity(city) {
   `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   // console.log(apiUrl);
   axios.get(apiUrl).then(refreshWeather);
-
-
 }
 
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-
-  searchCity(searchInput.value);
   
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
